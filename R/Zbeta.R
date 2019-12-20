a4161415c743a01a61045e212ba151c11c242b23
chorscroft <c.horscroft@soton.ac.uk>
2019-12-20 11:51:37 +0000

    First commit of the Zbeta function, including test files and manual entry

---
+++ R/Zbeta.R
@@ -0,0 +1,100 @@
+
+#' Runs the Zbeta function
+#'
+#' Returns a Zbeta value for each SNP location supplied to the function.
+#' For more information about the Zbeta statistic please see Jacobs (2016).
+#'
+#' @importFrom stats cor
+#'
+#' @param pos A numeric vector of SNP locations
+#' @param x A matrix of SNP values. Columns represent chromosomes, rows are SNP locations. Hence, the number of rows should equal the length of the `pos` vector. SNPs should all be biallelic.
+#' @param ws The window size which the `Zbeta` statistic will be calculated over. This should be on the same scale as the `pos` vector.
+#' @param minRandL Minimum number of SNPs in each set R and L for the statistic to be calculated. Default is 4.
+#' @param minRL Minimum value for the product of the set sizes for R and L. Default is 25.
+#' @param X Optional. Specify a region of the chromosome to calculate Zbeta for in the format `c(startposition, endposition)`. The start position and the end position should be within the extremes of the positions given in the `pos` vector. If not supplied, the function will calculate Zbeta for every SNP in the `pos` vector.
+#'
+#' @return A data frame containing the SNP positions and the Zbeta values for those SNPs
+#' @export
+
+Zbeta <- function(pos, x, ws, minRandL = 4, minRL = 25, X = NULL) {
+  #Check things are in the correct format
+
+  #Check pos is a numeric vector
+  if (is.numeric(pos) ==FALSE || is.vector(pos)==FALSE){
+    stop("pos must be a numeric vector")
+  }
+  #Check x is a matrix
+  if (is.matrix(x)==FALSE){
+    stop("x must be a matrix")
+  }
+  #Check x has rows equal to the length of pos
+  if (length(pos) != nrow(x)){
+    stop("The number of rows in x must equal the number of SNP locations given in pos")
+  }
+  #Check SNPs are all biallelic
+  if(sum(apply(x,1,function(x){length(unique(x))}) != 2)>0){
+    stop("SNPs must all be biallelic")
+  }
+  #Check windowsize is a number greater than 0
+  if(is.numeric(ws) ==FALSE || ws <= 0){
+    stop("ws must be a number greater than 0")
+  }
+  #Check minRandL is 0 or greater
+  if(is.numeric(minRandL) ==FALSE || minRandL < 0){
+    stop("minRandL must be a number greater than or equal to 0")
+  }
+  #Check minRL is 0 or greater
+  if(is.numeric(minRL) ==FALSE || minRL < 0){
+    stop("minRL must be a number greater than or equal to 0")
+  }
+  #If X is specified, check it is in the correct format
+  if (is.null(X)==FALSE){
+    if(is.numeric(X)==FALSE || is.vector(X)==FALSE){
+      stop("X should be a numeric vector of length 2 e.g. c(100,200)")
+    } else {
+      if (length(X) != 2){
+        stop("X should be a numeric vector of length 2 e.g. c(100,200)")
+      } else {
+        # X is in the correct format
+        # Check that X will actually return a result (i.e. that the region specied by X overlaps with pos)
+        if ((length(pos[pos>=X[1] & pos <= X[2]])>0) == FALSE){
+          stop("The region specified by X is outside the region contained in the pos vector")
+        }
+      }
+    }
+  } else {
+    # Set X equal to the extremes of pos
+    X<-c(pos[1],pos[length(pos)])
+  }
+
+  #Change matrix x to numeric if it isn't already
+  if (is.numeric(x)==FALSE){
+    x<-matrix(as.numeric(factor(x)),nrow=dim(x)[1])
+  }
+
+  # Set up output data frame
+  outputDF<-data.frame(POS=pos[pos>=X[1] & pos <= X[2]],Zbeta=NA)
+
+
+  # Loop over each position in the output data frame and calculate Zbeta
+  for (i in 1:nrow(outputDF)){
+
+    # Current physical position in chromosome
+    currentPos<-outputDF$POS[i]
+
+    ## check L, R and LR
+    noL <- length(pos[pos>=currentPos-ws/2 & pos < currentPos]) ## Number of SNPs to the left of the current SNP
+    noR <- length(pos[pos<=currentPos+ws/2 & pos > currentPos]) ## Number of SNPs to the right of the current SNP
+    if  (noL < minRandL || noR < minRandL || noL*noR < minRL){
+      #NA
+      outputDF$Zbeta[i]<-NA
+    } else {
+      rsqSum<-sum((cor(t(x[pos>=currentPos-ws/2 & pos<=currentPos+ws/2,]))^2)[1:noL,(noL+2):(noL+noR+1)])
+      outputDF$Zbeta[i]<-rsqSum/(noL*noR)
+    }
+  }
+  if (sum(is.na(outputDF$Zbeta))==nrow(outputDF)){
+    warning("No Zbeta values were calculated, try reducing minRandL and minRL or increasing the window size")
+  }
+  return(outputDF)
+}
---
+++ tests/testthat/test-Zbeta.R
@@ -0,0 +1,218 @@
+
+## Create basic example data frame to check Zbeta with. 15 SNPs over 5 chromosomes.
+
+df<-data.frame(
+  SNP=c("SNP1","SNP2","SNP3","SNP4","SNP5","SNP6","SNP7","SNP8","SNP9","SNP10","SNP11","SNP12","SNP13","SNP14","SNP15"),
+  POS=c(100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500),
+  C1=c(1,1,2,1,2,1,1,2,1,2,1,2,1,1,1),
+  C2=c(2,2,1,2,1,2,1,2,2,2,1,2,1,1,2),
+  C3=c(2,1,2,2,2,1,1,2,2,1,2,2,1,1,2),
+  C4=c(1,1,2,1,2,2,1,1,1,1,1,2,2,2,2),
+  C5=c(1,1,2,1,2,1,2,1,1,1,1,1,2,1,1)
+)
+
+## test that Zbeta is calculated correctly
+
+test_that("Zbeta calcualtes Zbeta statistic correctly", {
+
+  expect_equal(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = NULL),
+               data.frame(
+                 POS=c(100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500),
+                 Zbeta=c(NA,NA,NA,NA,
+                          (10+5/18)/40,
+                          (10+35/36)/45,
+                          (11+103/144)/48,
+                          (12+73/144)/49,
+                          (11+83/144)/48,
+                          (11+17/48)/45,
+                          (10+65/144)/40,
+                          NA,NA,NA,NA)
+               ))
+})
+
+## Test the function with a different window size
+
+test_that("Zbeta calcualtes Zbeta statistic correctly with a different window size", {
+
+  expect_equal(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 1100, minRandL = 4, minRL = 25, X = NULL),
+               data.frame(
+                 POS=c(100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500),
+                 Zbeta=c(NA,NA,NA,NA,NA,
+                          (7+11/72)/25,
+                          (5+5/9)/25,
+                          (6+41/144)/25,
+                          (6+101/144)/25,
+                          (7+17/144)/25,
+                          NA,NA,NA,NA,NA)
+               ))
+})
+
+## Test the function with a character matrix as x
+
+test_that("Zbeta calcualtes Zbeta statistic correctly with character matrix", {
+
+  df1<-df
+  df1[df1==1]<-"A"
+  df1[df1==2]<-"B"
+  expect_equal(Zbeta(pos = df1$POS, x = as.matrix(df1[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = NULL),
+               data.frame(
+                 POS=c(100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500),
+                 Zbeta=c(NA,NA,NA,NA,
+                         (10+5/18)/40,
+                         (10+35/36)/45,
+                         (11+103/144)/48,
+                         (12+73/144)/49,
+                         (11+83/144)/48,
+                         (11+17/48)/45,
+                         (10+65/144)/40,
+                         NA,NA,NA,NA)
+               ))
+})
+
+## Test the function with X supplied as a parameter
+
+test_that("Zbeta calcualtes Zbeta statistic correctly with X supplied", {
+
+  expect_equal(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = c(700,900)),
+               data.frame(
+                 POS=c(700,800,900),
+                 Zbeta=c((11+103/144)/48,
+                         (12+73/144)/49,
+                         (11+83/144)/48)
+               ))
+})
+
+## Test the function with X supplied as a parameter outside of the region defined in pos
+
+test_that("Zbeta fails with an X supplied outside of the region defined in pos", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = c(7000,9000)),
+               "The region specified by X is outside the region contained in the pos vector")
+})
+
+## Test the function with X supplied as character
+
+test_that("Zbeta fails with an X supplied as a character", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = c("700bp","900bp")),
+               "X should be a numeric vector of length 2 e.g. c(100,200)",
+               fixed=TRUE)
+})
+
+## Test the function with X supplied as only one number
+
+test_that("Zbeta fails with an X supplied as only one number", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = 700),
+               "X should be a numeric vector of length 2 e.g. c(100,200)",
+               fixed=TRUE)
+})
+
+## Test the function with X supplied with too many numbers
+
+test_that("Zbeta fails with an X supplied with too many numbers", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = c(700,900,1100)),
+               "X should be a numeric vector of length 2 e.g. c(100,200)",
+               fixed=TRUE)
+})
+
+## Test the function with ws non-numeric
+
+test_that("Zbeta fails when ws is non-numeric", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = "3000bp", minRandL = 4, minRL = 25, X = NULL),
+               "ws must be a number greater than 0")
+})
+
+## Test the function with ws = 0
+
+test_that("Zbeta fails when ws is zero", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 0, minRandL = 4, minRL = 25, X = NULL),
+               "ws must be a number greater than 0")
+})
+
+## Test the function with positions non-numeric
+
+test_that("Zbeta fails when pos is non-numeric", {
+
+  expect_error(Zbeta(pos = paste0(df$POS,"bp"), x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = NULL),
+               "pos must be a numeric vector")
+})
+
+## Test the function with minLandR as non-numeric
+
+test_that("Zbeta fails when minLandR is non-numeric", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = "4snps", minRL = 25, X = NULL),
+               "minRandL must be a number greater than or equal to 0")
+})
+
+## Test the function with minLandR as negative
+
+test_that("Zbeta fails when minLandR is negative", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = -1, minRL = 25, X = NULL),
+               "minRandL must be a number greater than or equal to 0")
+})
+
+## Test the function with minLR as non-numeric
+
+test_that("Zbeta fails when minLR is non-numeric", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = "25b", X = NULL),
+               "minRL must be a number greater than or equal to 0")
+})
+
+## Test the function with minLR as negative
+
+test_that("Zbeta fails when minLR is negative", {
+
+  expect_error(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = -25, X = NULL),
+               "minRL must be a number greater than or equal to 0")
+})
+
+## Test the function with x not a matrix
+
+test_that("Zbeta fails when x is not a matrix", {
+
+  expect_error(Zbeta(pos = df$POS, x = df[,3:7], ws  = 3000, minRandL = 4, minRL = 25, X = NULL),
+               "x must be a matrix")
+})
+
+## Test the function with x not having the correct amount of rows
+
+test_that("Zbeta fails when the number of rows in x is not equal to the length of pos", {
+
+  expect_error(Zbeta(pos = df$POS, x = t(as.matrix(df[,3:7])), ws  = 3000, minRandL = 4, minRL = 25, X = NULL),
+               "The number of rows in x must equal the number of SNP locations given in pos")
+})
+
+## Test the function with a SNP having only one allele
+
+test_that("Zbeta fails when a SNP has only one allele", {
+
+  df1<-df
+  df1[1,3:7]<-1
+  expect_error(Zbeta(pos = df1$POS, x = as.matrix(df1[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = NULL),
+               "SNPs must all be biallelic")
+})
+
+## Test the function with a SNP having more than two alleles
+
+test_that("Zbeta fails when a SNP has more than two alleles", {
+
+  df1<-df
+  df1[1,7]<-3
+  expect_error(Zbeta(pos = df1$POS, x = as.matrix(df1[,3:7]), ws  = 3000, minRandL = 4, minRL = 25, X = NULL),
+               "SNPs must all be biallelic")
+})
+
+## Test the warning that all values returned are NA by increasing minRL
+
+test_that("Zbeta warns about all NAs", {
+
+  expect_warning(Zbeta(pos = df$POS, x = as.matrix(df[,3:7]), ws  = 3000, minRandL = 4, minRL = 50, X = NULL),
+                 "No Zbeta values were calculated, try reducing minRandL and minRL or increasing the window size")
+})
