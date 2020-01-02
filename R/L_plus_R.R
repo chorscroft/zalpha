51a5c737630fc47e23571aced5d679e459d49b40
chorscroft <c.horscroft@soton.ac.uk>
2020-01-02 19:25:27 +0000

    Renamed the LplusR function to L_plus_R

--- R/L_plus_R.R
+++ R/L_plus_R.R
@@ -1,21 +1,21 @@
 
-#' Runs the LplusR function
+#' Runs the L_plus_R function
 #'
 #' Returns the \eqn{{|L| \choose 2} + {|R| \choose 2}}{(|L| choose 2) + (|R| choose 2)} value for each SNP location supplied to the function.
-#' For more information about the LplusR diversity statistic please see Jacobs (2016).
+#' For more information about the L_plus_R diversity statistic please see Jacobs (2016).
 #'
 #'
 #'
 #' @param pos A numeric vector of SNP locations
 #' @param x A matrix of SNP values. Columns represent chromosomes, rows are SNP locations. Hence, the number of rows should equal the length of the \code{pos} vector. SNPs should all be biallelic.
-#' @param ws The window size which the \code{LplusR} statistic will be calculated over. This should be on the same scale as the \code{pos} vector.
-#' @param X Optional. Specify a region of the chromosome to calculate LplusR for in the format \code{c(startposition, endposition)}. The start position and the end position should be within the extremes of the positions given in the \code{pos} vector. If not supplied, the function will calculate LplusR for every SNP in the \code{pos} vector.
+#' @param ws The window size which the \code{L_plus_R} statistic will be calculated over. This should be on the same scale as the \code{pos} vector.
+#' @param X Optional. Specify a region of the chromosome to calculate L_plus_R for in the format \code{c(startposition, endposition)}. The start position and the end position should be within the extremes of the positions given in the \code{pos} vector. If not supplied, the function will calculate L_plus_R for every SNP in the \code{pos} vector.
 #'
-#' @return A data frame containing the SNP positions and the LplusR values for those SNPs
+#' @return A data frame containing the SNP positions and the L_plus_R values for those SNPs
 #' @references Jacobs, G.S., T.J. Sluckin, and T. Kivisild, \emph{Refining the Use of Linkage Disequilibrium as a Robust Signature of Selective Sweeps.} Genetics, 2016. \strong{203}(4): p. 1807
 #' @export
 
-LplusR <- function(pos, x, ws, X = NULL) {
+L_plus_R <- function(pos, x, ws, X = NULL) {
   #Check things are in the correct format
 
   #Check pos is a numeric vector
@@ -58,28 +58,28 @@ LplusR <- function(pos, x, ws, X = NULL) {
     X<-c(pos[1],pos[length(pos)])
   }
 
-  #Change matrix x to numeric if it isn't aLplusReady
+  #Change matrix x to numeric if it isn't aL_plus_Ready
   if (is.numeric(x)==FALSE){
     x<-matrix(as.numeric(factor(x)),nrow=dim(x)[1])
   }
 
   # Set up output data frame
-  outputDF<-data.frame(POS=pos[pos>=X[1] & pos <= X[2]],LplusR=NA)
+  outputDF<-data.frame(POS=pos[pos>=X[1] & pos <= X[2]],L_plus_R=NA)
 
 
-  # Loop over each position in the output data frame and calculate LplusR
+  # Loop over each position in the output data frame and calculate L_plus_R
   for (i in 1:nrow(outputDF)){
 
     # Current physical position in chromosome
     currentPos<-outputDF$POS[i]
 
-    ## get L, R and LplusR
+    ## get L, R and L_plus_R
     noL <- length(pos[pos>=currentPos-ws/2 & pos < currentPos]) ## Number of SNPs to the left of the current SNP
     noR <- length(pos[pos<=currentPos+ws/2 & pos > currentPos]) ## Number of SNPs to the right of the current SNP
     if(noL < 2 || noR < 2){  #Must be at least 2 to calculate n choose 2
-      outputDF$LplusR[i]<-NA
+      outputDF$L_plus_R[i]<-NA
     } else {
-      outputDF$LplusR[i]<-choose(noL,2)+choose(noR,2)
+      outputDF$L_plus_R[i]<-choose(noL,2)+choose(noR,2)
     }
   }
   return(outputDF)
