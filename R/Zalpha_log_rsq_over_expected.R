a4d94954963484dff421c9a664d9d01d83dd5259
chorscroft <c.horscroft@soton.ac.uk>
2020-01-07 16:32:32 +0000

    Added "log of" to the title of the documentation

--- R/Zalpha_log_rsq_over_expected.R
+++ R/Zalpha_log_rsq_over_expected.R
@@ -1,6 +1,6 @@
 
 
-#' Runs the Zalpha function on the r squared values over the expected r squared values for the region
+#' Runs the Zalpha function on the log of the r squared values over the expected r squared values for the region
 #'
 #' Returns a \eqn{Z_{\alpha}^{log_{10}(r^2/E[r^2])}}{Zalpha} value for each SNP location supplied to the function, based on
 #' the expected \eqn{r^2} values given an LD profile and cM distances.
