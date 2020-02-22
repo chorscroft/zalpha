0681a45436f911026be302548250743c259dac79
chorscroft <c.horscroft@soton.ac.uk>
2020-02-22 10:53:27 +0000

    Add two example datasets "snps" and "LDprofile" including relevant documentation, and therefore adding R>=2.10 as a dependency

---
+++ R/snps-data.R
@@ -0,0 +1,25 @@
+#' Details about some simulated SNPs
+#'
+#' A dataset containing the positions, genetic distances and
+#' alleles for 20 SNPs, across 10 simulated chromosomes.
+#'
+#' @docType data
+#'
+#' @usage data(snps)
+#'
+#' @format A data frame with 20 rows and 12 variables:
+#' \describe{
+#'   \item{positions}{location of the SNP on the chromosome}
+#'   \item{distances}{genetic distance of the SNP from the start of the chromosome}
+#'   \item{chrom_1}{allele of the SNP on the first example chromosome}
+#'   \item{chrom_2}{allele of the SNP on the second example chromosome}
+#'   \item{chrom_3}{allele of the SNP on the third example chromosome}
+#'   \item{chrom_4}{allele of the SNP on the fourth example chromosome}
+#'   \item{chrom_5}{allele of the SNP on the fifth example chromosome}
+#'   \item{chrom_6}{allele of the SNP on the sixth example chromosome}
+#'   \item{chrom_7}{allele of the SNP on the seventh example chromosome}
+#'   \item{chrom_8}{allele of the SNP on the eighth example chromosome}
+#'   \item{chrom_9}{allele of the SNP on the ninth example chromosome}
+#'   \item{chrom_10}{allele of the SNP on the tenth example chromosome}
+#' }
+"snps"
